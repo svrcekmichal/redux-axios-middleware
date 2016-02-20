2a161cf026f6688dbd00c51f4787312637e65f78
Michal Svrček <svrcekmichal@gmail.com>
2016-02-20 11:59:10 +0100

    fixed issue with promise resolving to soon

--- src/middleware.js
+++ src/middleware.js
@@ -1,21 +1,26 @@
 import * as defaultOptions from './defaults';
 import {getActionTypes} from './helpers';
 
-export default (options) => ({getState,dispatch}) => next => action => {
-  const {client,onSuccess,onError,onComplete} = {...defaultOptions,...options};
+export default (client, options = {}) => {
 
-  if(!action.payload || !action.payload.request) {
-    return next(action);
+  let {onSuccess,onError,onComplete} = {...defaultOptions,...options};
+
+  return ({getState,dispatch}) => next => action => {
+
+    if(!action.payload || !action.payload.request) {
+      return next(action);
+    }
+
+    const [REQUEST] = getActionTypes(action);
+    next({...action, type:REQUEST});
+
+    return client.request(action.payload.request).then((response) => {
+      const newAction = onSuccess({action,next, response,getState,dispatch});
+      onComplete({action:newAction,next, getState,dispatch});
+    }, (error) => {
+      const newAction = onError({action,next,error,getState,dispatch});
+      onComplete({action:newAction,next, getState,dispatch});
+    });
   }
 
-  const [REQUEST] = getActionTypes(action);
-  next({...action, type:REQUEST});
-
-  return client.request(action.payload.request).then((response) => {
-    const newAction = onSuccess({action,next, response,getState,dispatch});
-    return onComplete({action:newAction,next, getState,dispatch});
-  }, (error) => {
-    const newAction = onError({action,next,error,getState,dispatch});
-    return onComplete({action:newAction,next, getState,dispatch});
-  });
 }
