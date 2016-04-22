b47ce4a4515e09a9bb527cba415ec1314abd00c9
Nathan Maves <nathan.maves@gmaill.com>
2016-04-21 21:42:22 -0600

    Adding bound interceptors for axios.

--- src/middleware.js
+++ src/middleware.js
@@ -1,11 +1,31 @@
 import * as defaultOptions from './defaults';
 import { getActionTypes } from './getActionTypes';
+import axios from 'axios';
+
+let interceptorsBound = false;
+
+function bindInterceptors(getState, { requestInterceptors = [], responseInterceptors = [] } = {}) {
+  requestInterceptors.forEach((interceptor) => {
+    axios.interceptors.request.use(interceptor.bind(null, getState));
+  });
+
+  responseInterceptors.forEach((interceptor) => {
+    axios.interceptors.response.use(interceptor.bind(null, getState));
+  });
+
+  interceptorsBound = true;
+}
 
 export default (client, userOptions = {}) => {
   const options = { ...defaultOptions, ...userOptions };
+
   return ({ getState, dispatch }) => next => action => {
     if (!options.isAxiosRequest(action)) {
       return next(action);
+    }
+
+    if (options.interceptors && !interceptorsBound) {
+      bindInterceptors(getState, options.interceptors);
     }
     const [REQUEST] = getActionTypes(action, options);
     next({ ...action, type: REQUEST });
