562296b3a34f6647528774e67f22f6a1cc0698b9
yosef <maulerik@gmail.com>
2017-08-28 17:39:43 +0300

    added more types

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,11 @@
-export interface axiosMiddleware {
-  ERROR_SUFFIX: string;
-  SUCCESS_SUFFIX: string;
+import axios, {AxiosInstance, AxiosError, AxiosResponse} from 'axios';
+
+export interface suffixTypes {
+  errorSuffix: string;
+  successSuffix: string;
+}
+
+export interface axiosMiddleware extends suffixTypes {
   default: (client: any, customMiddleWareOptions: any, customClientOptions: any) => any;
   getActionTypes: (action: any) => any;
   multiClientMiddleware: (client: any, customMiddleWareOptions: any) => any;
@@ -10,6 +15,26 @@ export interface axiosMiddleware {
 
 
 declare const axiosMiddleware: axiosMiddleware;
+
+export function multiClientMiddleware(clients?: any, customMiddlewareOptions?: any): any;
+
+export const returnRejectedPromiseOnError: boolean;
+
+export const defaultClientName: string;
+
+export const isAxiosRequest: boolean;
+
+export const getRequestConfig: AxiosResponse;
+
+export const getClientName: AxiosInstance;
+
+export const getRequestOptions: any;
+
+export function onSuccess({action, next, response}?: any, options?: any): any;
+
+export function onError({action, next, response}?: any, options?: any): any;
+
+export function onComplete(): any;
 
 
 export default axiosMiddleware;
